9e7ffe5df72a51aeb520750a71545ed9ec1d04d1
Ferenc Böröczki <f3c0@users.noreply.github.com>
2020-01-22 21:21:48 +0100

    introduce TTL

--- src/echc.erl
+++ src/echc.erl
@@ -1,12 +1,18 @@
 -module(echc).
 
--export([start/2, get/2, set/3]).
+-export([start/2, get/2, set/3, get/3, set/4]).
 
 start(Name, Getter) ->
   echc_sup:start_child(Name, Getter).
 
 get(Name, Key) ->
-  echc_srv:get(Name, Key).
+  get(Name, Key, infinity).
+
+get(Name, Key, TTL) ->
+  echc_srv:get(Name, Key, TTL).
 
 set(Name, Key, Value) ->
-  echc_srv:set(Name, Key, Value).
+  set(Name, Key, Value, infinity).
+
+set(Name, Key, Value, TTL) ->
+  echc_srv:set(Name, Key, Value, TTL).
--- src/echc_srv.erl
+++ src/echc_srv.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/2, get/2, set/3]).
+-export([start_link/2, get/3, set/4]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
@@ -27,11 +27,11 @@
 start_link(Name, Getter) ->
   gen_server:start_link({local, Name}, ?MODULE, [Getter], []).
 
-get(Server, Key) ->
-  gen_server:call(Server, {get, Key}).
+get(Server, Key, TTL) ->
+  gen_server:call(Server, {get, Key, TTL}).
 
-set(Server, Key, Value) ->
-  gen_server:cast(Server, {set, Key, Value}).
+set(Server, Key, Value, TTL) ->
+  gen_server:cast(Server, {set, Key, Value, TTL}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -55,15 +55,15 @@ init([Getter]) ->
   {noreply, NewState :: #state{}, timeout() | hibernate} |
   {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
   {stop, Reason :: term(), NewState :: #state{}}).
-handle_call({get, Key}, From,
+handle_call({get, Key, TTL}, From,
     State = #state{cache = Cache, queue = Queue, getter = Getter}) ->
-  case maps:find(Key, Cache) of
+  case cache_get(Key, Cache) of
     {ok, Value} -> {reply, Value, State};
-    error ->
+    not_found ->
       case maps:get(Key, Queue, []) of
         [] ->
           Server = self(),
-          spawn_link(fun() -> gen_server:cast(Server, {set, Key, Getter(Key)}) end),
+          spawn_link(fun() -> gen_server:cast(Server, {set, Key, Getter(Key), TTL}) end),
           {noreply, State#state{queue = Queue#{Key => [From]}}};
         Targets -> {noreply, State#state{queue = Queue#{Key => [From | Targets]}}}
       end
@@ -71,15 +71,28 @@ handle_call({get, Key}, From,
 handle_call(_Request, _From, State = #state{}) ->
   {reply, ok, State}.
 
+cache_get(Key, Cache) ->
+  Now = erlang:system_time(millisecond),
+  case maps:find(Key, Cache) of
+    {ok, {infinity, Value}} -> {ok, Value};
+    {ok, {TTL, Value}} when TTL >= Now -> {ok, Value};
+    _ -> not_found
+  end.
+
+cache_set(Key, Value, Cache, infinity) ->
+  Cache#{Key => {infinity, Value}};
+cache_set(Key, Value, Cache, TTL) ->
+  Cache#{Key => {erlang:system_time(millisecond) + TTL, Value}}.
+
 %% @private
 %% @doc Handling cast messages
 -spec(handle_cast(Request :: term(), State :: #state{}) ->
   {noreply, NewState :: #state{}} |
   {noreply, NewState :: #state{}, timeout() | hibernate} |
   {stop, Reason :: term(), NewState :: #state{}}).
-handle_cast({set, Key, Value}, State = #state{cache = Cache, queue = Queue}) ->
+handle_cast({set, Key, Value, TTL}, State = #state{cache = Cache, queue = Queue}) ->
   [gen_server:reply(Target, Value) || Target <- maps:get(Key, Queue, [])],
-  {noreply, State#state{cache = Cache#{Key => Value}, queue = Queue#{Key => []}}};
+  {noreply, State#state{cache = cache_set(Key, Value, Cache, TTL), queue = Queue#{Key => []}}};
 handle_cast(_Request, State = #state{}) ->
   {noreply, State}.
 
